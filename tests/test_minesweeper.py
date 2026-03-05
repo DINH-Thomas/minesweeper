1d5513d54bcd1df9c45a651a4725c05d2aaa1876
Thomas Dinh <th_dinh@yahoo.fr>
2026-03-05 15:29:52 +0100

    Commit minesweeper avec precommit

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,8 +1,11 @@
 import pytest
+
 from src import minesweeper
+
 
 def test_module_exists():
     assert minesweeper
+
 
 def test_place_mines():
     game = minesweeper.Minesweeper(3, 3, 2)
@@ -10,8 +13,10 @@ def test_place_mines():
     # TODO : Add assertions
     assert len(game.mines) == 2
 
+
 def test_reveal():
     import random
+
     random.seed(0)
     game = minesweeper.Minesweeper(3, 3, 2)
     game.place_mines()
@@ -19,12 +24,14 @@ def test_reveal():
     # TODO : Add assertions
     assert game.revealed == {(2, 2)}
 
+
 def test_get_board():
     game = minesweeper.Minesweeper(3, 3, 2)
     game.place_mines()
     board = game.get_board()
     # TODO : Add assertions
     assert isinstance(board, list)
+
 
 def test_is_winner():
     game = minesweeper.Minesweeper(3, 3, 2)
@@ -35,6 +42,7 @@ def test_is_winner():
             if (r, c) not in game.mines:
                 game.reveal(r, c)
     assert game.is_winner() == True
+
 
 def test_restart():
     game = minesweeper.Minesweeper(3, 3, 2)
